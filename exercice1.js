31dd54ef67e3c4b77ec687cafdf17bc2fecc0768
lilia <lilianom59@outlook.fr>
2022-12-10 12:30:54 +0100

    stopwatch

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,18 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
+let a = 0;
 let countUp = function() {
-    // ?
+    let counter = setTimeout(function() {
+        seconds.innerHTML++
+        (a <= 60) ? countUp() : null;
+    }, 1000)
 };
+
+countUp();
 
 // How can you make it stop counting?
 let stopCountUp = function() {
+    countUp = null
 };
 let stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
